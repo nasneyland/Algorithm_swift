03fee8e192c4ad72610a08584d9093766d60aa37
Najin Shin <63686155+skwls2087@users.noreply.github.com>
2023-01-09 21:14:33 +0900

    feat: 기본함수-String 연습

--- cote/main.swift
+++ cote/main.swift
@@ -7,4 +7,23 @@
 
 import Foundation
 
-print("hello")
+// ------------------------------------------------------------------------------------------
+// MARK: String
+
+let str = "abcdefghijklmnopqrstuvwxyz"
+
+print(str.firstIndex(of: "e")!.utf16Offset(in: str))
+print(str[str.index(str.startIndex, offsetBy: 8)])
+let start = str.index(str.startIndex, offsetBy: 4)
+let end = str.index(str.startIndex, offsetBy: 8)
+print(str[start...end])
+print(str[str.startIndex])
+print(str[str.startIndex])
+print(str[str.index(after: str.startIndex)])
+print(str[str.index(str.startIndex, offsetBy: str.count - 1)])
+print(str[str.index(before: str.endIndex)])
+print(str.contains("efg"))
+let arr = str.map{String($0)}
+print(arr)
+print(arr.reduce("", +))
+print(arr.joined())
