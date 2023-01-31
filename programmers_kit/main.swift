2ebb4204715c916743b9100595a7f3b2cae90048
Najin Shin <63686155+skwls2087@users.noreply.github.com>
2023-01-31 21:15:01 +0900

    feat: 프로그래머스_그리디_4문제

--- programmers_kit/main.swift
+++ programmers_kit/main.swift
@@ -10,7 +10,8 @@
 
 import Foundation
 
-print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
+print(solution("1231234", 3))
+//print(solution("477252841", 4))
 
 // ------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------
@@ -30,7 +31,7 @@ print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 
 //}
 
 // ------------------------------------------------------------------------------------------
-// MARK: 베스트앨범 (Lv.2) (딕셔너리)
+// MARK: 베스트앨범 (Lv.2) (딕셔너리) (2차원 고차함수 이용하기)
 // 스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다. 1)속한 노래가 많이 재생된 장르를 먼저 수록합니다. 2)장르 내에서 많이 재생된 노래를 먼저 수록합니다. 3)장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다. 노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
 
 //func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
@@ -95,31 +96,185 @@ print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 
 
 //func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
 //
-////    // Set을 이용한 풀이
-////    var lostSet = Set(lost.sorted(by: <)).subtracting(reserve)
-////    let reserveSet = Set(reserve.sorted(by: <)).subtracting(lost)
-////
-////    for reserve in reserveSet {
-////        if lostSet.contains(reserve - 1) {
-////            lostSet.remove(reserve - 1)
-////            continue
-////        } else if lostSet.contains(reserve + 1) {
-////            lostSet.remove(reserve + 1)
-////        }
-////    }
+//    // Set을 이용한 풀이
+//    var lostSet = Set(lost.sorted(by: <)).subtracting(reserve)
+//    let reserveSet = Set(reserve.sorted(by: <)).subtracting(lost)
 //
-////    // 배열을 이용한 풀이 - 내 테스트케이스로는 틀리는데 프로그래머스에서는 이게 맞다고 된다.
-////    let newReserve = reserve.filter { !lost.contains($0) }
-////    let newLost = lost.filter { !reserve.contains($0) }
-////
-////    var lostPeople: Int = newLost.count
-////
-////    newReserve.forEach {
-////        let isLend: Bool = newLost.contains($0 - 1) || newLost.contains($0 + 1)
-////        if isLend && lostPeople > 0 {
-////            lostPeople -= 1
-////        }
-////    }
+//    for reserve in reserveSet {
+//        if lostSet.contains(reserve - 1) {
+//            lostSet.remove(reserve - 1)
+//            continue
+//        } else if lostSet.contains(reserve + 1) {
+//            lostSet.remove(reserve + 1)
+//        }
+//    }
+
+//    // 배열을 이용한 풀이
+//    let newReserve = reserve.filter { !lost.contains($0) }
+//    let newLost = lost.filter { !reserve.contains($0) }
+//
+//    var lostPeople: Int = newLost.count
+//
+//    newReserve.forEach {
+//        let isLend: Bool = newLost.contains($0 - 1) || newLost.contains($0 + 1)
+//        if isLend && lostPeople > 0 {
+//            lostPeople -= 1
+//        }
+//    }
 //
 //    return (n - lostPeople)
+//}
+
+//func solution(_ n: Int, _ lost: [Int], _ reserve: [Int]) -> Int {
+//    var array = Array(repeating: 1, count: n)
+//    lost.forEach { array[$0-1] -= 1 }
+//    reserve.forEach { array[$0-1] += 1 }
+//    (0...n-1).forEach {
+//        if $0 != 0 && array[$0] == 0 && array[$0-1] == 2 {
+//            // 앞사람 확인
+//            array[$0] = 1
+//            array[$0-1] = 1
+//        } else if $0 != (n-1) && array[$0] == 0 && array[$0+1] == 2 {
+//            // 뒷사람 확인
+//            array[$0] = 1
+//            array[$0+1] = 1
+//        }
+//    }
+//    return array.filter{$0>0}.count
+//}
+
+// ------------------------------------------------------------------------------------------
+// MARK: 조이스틱 (Lv.2)
+// 조이스틱으로 알파벳 이름을 완성하세요. 맨 처음엔 A로만 이루어져 있습니다. ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA
+
+//func solution(_ name:String) -> Int {
+//    let name = Array(name)
+//    let aValue = Int(Character("A").asciiValue!)
+//    let zValue = Int(Character("Z").asciiValue!)
+//
+//    var updown = 0
+//    var leftright = name.count-1
+//
+//    for startIdx in 0..<name.count {
+//        updown += min(Int(name[startIdx].asciiValue!) - aValue, zValue - Int(name[startIdx].asciiValue!) + 1)
+//
+//        var endIdx = startIdx + 1
+//        while endIdx < name.count && name[endIdx] == "A" {
+//            endIdx += 1
+//        }
+//
+//        let moveFront = startIdx * 2 + (name.count - endIdx)
+//        let moveBack = (name.count - endIdx) * 2 + startIdx
+//
+//        leftright = min(leftright, moveFront)
+//        leftright = min(leftright, moveBack)
+//    }
+//
+//    return updown + leftright
+//}
+
+// ------------------------------------------------------------------------------------------
+// MARK: 큰 수 만들기 (Lv.2)
+// 어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다. 예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다. 문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
+
+//// 내가 처음 푼 함수 - 시간 초과 뜬 함수
+//func solution(_ number:String, _ k:Int) -> String {
+//    var numbers = number.map{String($0)}
+//    var result = ""
+//    var removeCnt = 0
+//
+//    while removeCnt != k {
+//        let max = numbers[0...(k-removeCnt)].max()!
+//        let index = numbers.firstIndex(of: max)!
+//
+//        numbers.removeSubrange(0...index)
+//        result += max
+//        removeCnt += index
+//    }
+//    return result + numbers.joined()
+//}
+
+//// 스택을 이용한 함수
+//func solution(_ number: String, _ k: Int) -> String {
+//    var result: [Int] = []
+//    var count: Int = 0
+//
+//    Array(number).compactMap { Int(String($0)) }.forEach { item in
+//        while count < k {
+//            if let last = result.last, last < item {
+//                result.removeLast()
+//                count += 1
+//            } else {
+//                break
+//            }
+//        }
+//        result.append(item)
+//    }
+//
+//    return result[0..<(result.count - k + count)].map { "\($0)" }.joined()
+//}
+
+// ------------------------------------------------------------------------------------------
+// MARK: 섬 연결하기 (Lv.3)
+// n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때, 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요. 다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다. 예를 들어 A 섬과 B 섬 사이에 다리가 있고, B 섬과 C 섬 사이에 다리가 있으면 A 섬과 C 섬은 서로 통행 가능합니다.
+
+//func solution(_ n:Int, _ costs:[[Int]]) -> Int {
+//  func findParent(_ node: Int) -> Int {
+//    if parent[node] != node {
+//      return findParent(parent[node])
+//    } else {
+//      return node
+//    }
+//  }
+//  
+//  func union(_ a: Int, _ b: Int) {
+//    let a = findParent(a)
+//    let b = findParent(b)
+//    
+//    if a < b {
+//      parent[b] = a
+//    } else {
+//      parent[a] = b
+//    }
+//  }
+//  
+//  var totalCost = 0
+//  var parent = Array(repeating: 0, count: n+1)
+//  (1...n).forEach { parent[$0] = $0 }
+//  
+//  for cost in costs.sorted(by: { $0[2] < $1[2] }) {
+//    let (a, b, dist) = (cost[0], cost[1], cost[2])
+//    
+//    if findParent(a) != findParent(b) {
+//      union(a, b)
+//      totalCost += dist
+//    }
+//  }
+//  
+//  return totalCost
+//}
+//
+//func solution(_ n:Int, _ costs:[[Int]]) -> Int {
+//    var sortedCosts = costs.sorted(by: {$0[2] < $1[2]})
+//    var connect: [Int] = [sortedCosts[0][0], sortedCosts[0][1]]
+//    var answer: Int = sortedCosts[0][2]
+//
+//    while(connect.count < n) {
+//        for i in 0 ..< sortedCosts.count {
+//            let cost = sortedCosts[i]
+//            if connect.contains(cost[0]) && !connect.contains(cost[1]) {
+//                connect.append(cost[1])
+//                answer += cost[2]
+//                sortedCosts.remove(at: i)
+//                break
+//            } else if !connect.contains(cost[0]) && connect.contains(cost[1]) {
+//                connect.append(cost[0])
+//                answer += cost[2]
+//                sortedCosts.remove(at: i)
+//                break
+//            }
+//        }
+//    }
+//
+//    return answer
 //}
