e00dbe59abf9ef8266ddef38fb14955e63ab99dd
Najin Shin <63686155+skwls2087@users.noreply.github.com>
2023-02-02 17:35:36 +0900

    feat: 카카오 기출 - 주차 요금 계산

--- kakao/main.swift
+++ kakao/main.swift
@@ -10,7 +10,7 @@
 
 import Foundation
 
-print(solution(437674, 3))
+print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
 
 // ------------------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------------------
@@ -123,6 +123,44 @@ print(solution(437674, 3))
 //        }
 //        return (2...Int(sqrt(Double(num)))).filter { num % $0 == 0 }.count == 0
 //    }.count
+//}
+
+// MARK: 3. 주차 요금 계산
+
+//func solution(_ fees:[Int], _ records:[String]) -> [Int] {
+//    var carDic = [String: [[String]]]()
+//    
+//    // 차량 별 입출차 정보 딕셔너리에 담기
+//    records.forEach { record in
+//        let car = record.components(separatedBy: " ")
+//        if car[2] == "OUT" {
+//            carDic[car[1]]![carDic[car[1]]!.count-1].append(car[0])
+//        } else if carDic[car[1]] == nil {
+//            carDic[car[1]] = [[car[0]]]
+//        } else {
+//            carDic[car[1]]!.append([car[0]])
+//        }
+//    }
+//    
+//     // 시각 사이의 시간을 구해주는 함수
+//     func timeInterval(_ start: String, _ end: String) -> Int {
+//         let s = start.split(separator: ":").map{Int($0)!}
+//         let e = end.split(separator: ":").map{Int($0)!}
+//         return ((e[1] + e[0] * 60)-(s[1] + s[0] * 60))
+//     }
+//    
+//     // 시간 별 요금
+//     func timeFee(_ time: Int) -> Int {
+//         if time <= fees[0] {
+//             return fees[1]
+//         } else {
+//             return fees[1] + (Int(ceil(Double(time - fees[0]) / Double(fees[2]))) * fees[3])
+//         }
+//     }
+//    
+//    return carDic.sorted{$0.0 < $1.0}.map { onoff in
+//         return timeFee(onoff.1.reduce(0) {$0 + timeInterval($1[0], $1.count == 1 ? "23:59" : $1[1])})
+//     }
 //}
 
 // ------------------------------------------------------------------------------------------
