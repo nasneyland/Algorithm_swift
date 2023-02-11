e5d313afc83348d5376ff36cb29d388d886f464e
Najin Shin <113892130+nasneyland@users.noreply.github.com>
2023-02-11 23:29:11 +0900

    feat: 백준 클래스1

--- baekjoon/main.swift
+++ baekjoon/main.swift
@@ -7,6 +7,40 @@
 
 import Foundation
 
-let input = readLine()!
+solution()
 
-print(input.count)
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+// MARK: - 클래스 1
+// ------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------
+
+// MARK: 단어공부
+
+//func solution(){
+//    let input = readLine()!
+//
+//    var dic = [String: Int]()
+//    input.forEach {
+//        dic[String($0).uppercased(), default: 0] += 1
+//    }
+//
+//    let sortedDic = dic.sorted{$0.1 > $1.1}
+//    print(sortedDic.count > 1 && sortedDic[0].1 == sortedDic[1].1 ? "?" : sortedDic[0].0)
+//}
+
+// MARK: 아스키코드
+
+//func solution() {
+//    let input = readLine()!
+//    print(Character(input).asciiValue!)
+//}
+
+// MARK: 음계
+
+func solution() {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    input.enumerated().forEach { (i, num) in
+        
+    }
+}
